322a1b02d63898227b197ebe652fd5bf9466e735
premanshum <32505482+premanshum@users.noreply.github.com>
2023-11-23 16:26:56 +0530

    Update main.go

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,1 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+
+	countLines(os.Stdin, counts)
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
 
