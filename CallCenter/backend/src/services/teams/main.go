fb761c0ec0e9f6f83c199fadc37b2d0b9f9e94a3
prem-mukherji <premanshu.mukherji@maersk.comm>
2024-01-14 15:01:08 +0530

    Dockerfile added; sample taems.json file added;

---
+++ CallCenter/backend/src/services/teams/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Define a complex data structure
+type ApiResponse struct {
+	Status  string        `json:"status"`
+	Message string        `json:"message"`
+	Data    []interface{} `json:"data"`
+}
+
+// Another struct for a specific type of data
+type UserData struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+func main() {
+	SetupServer().Run()
+}
+
+func SetupServer() *gin.Engine {
+	r := gin.Default()
+	r.GET("/", HealthHandler)
+	r.GET("/teams", GetTeamsHandler)
+	return r
+}
+
+func HealthHandler(c *gin.Context) {
+	response := ApiResponse{
+		Status:  os.Getenv("NEWVAR"),
+		Message: "Web-app is healthy",
+	}
+
+	c.JSON(200, response)
+}
+
+func GetTeamsHandler(c *gin.Context) {
+
+	// Open jsonFile
+	jsonFile, err := os.Open("teams.json")
+	// if os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened users.json")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	byteValue, _ := io.ReadAll(jsonFile)
+
+	var result map[string]interface{}
+
+	json.Unmarshal([]byte(byteValue), &result)
+
+	array := result["teams"].([]interface{})
+
+	fmt.Println(array)
+
+	for i := range array {
+		fmt.Println(array[i])
+		//fmt.Printf("key=%s value=%+v\n ", k, v)
+	}
+
+	response := ApiResponse{
+		Status:  os.Getenv("NEWVAR"),
+		Message: "Data retrieved successfully",
+		Data:    array,
+	}
+
+	c.JSON(200, response)
+}
+
+func GetTeamsHandler1(c *gin.Context) {
+	var userlist []interface{}
+
+	userlist = append(userlist,
+		UserData{
+			Username: "prem",
+			Email:    "prem@example.com",
+		})
+
+	userlist = append(userlist,
+		UserData{
+			Username: "priya",
+			Email:    "priya@example.com",
+		})
+
+	response := ApiResponse{
+		Status:  os.Getenv("NEWVAR"),
+		Message: "Data retrieved successfully",
+		Data:    userlist,
+	}
+
+	c.JSON(200, response)
+}
--- helloworld/main.go
+++ helloworld/main.go
@@ -4,18 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"unicode"
 )
 
 func main() {
-	counts := make(map[string]int)
+	//counts := make(map[string]int)
 
-	countLines(os.Stdin, counts)
+	// countLines(os.Stdin, counts)
 
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
-		}
-	}
+	// for line, n := range counts {
+	// 	if n > 1 {
+	// 		fmt.Printf("%d\t%s\n", n, line)
+	// 	}
+	// }
+
+	ReadFile("inputfile.txt")
 }
 
 func countLines(f *os.File, counts map[string]int) {
@@ -25,3 +28,51 @@ func countLines(f *os.File, counts map[string]int) {
 	}
 }
 
+func ReadFile(filename string) {
+	readFile, err := os.Open(filename)
+	check(err)
+	defer readFile.Close()
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	counts := make(map[string]int)
+	bigInt := 0
+	for fileScanner.Scan() {
+		counts[fileScanner.Text()] = findFirstDigit(fileScanner.Text())*10 + findLastDigit(fileScanner.Text())
+		bigInt += counts[fileScanner.Text()]
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+
+	fmt.Printf("\n\nTotal:%d", bigInt)
+}
+
+func findFirstDigit(str string) int {
+	for _, char := range str {
+		if unicode.IsDigit(char) {
+			return int(char - '0')
+		}
+	}
+	return 0
+}
+
+func findLastDigit(str string) int {
+	last := len(str) - 1
+	for i := range str {
+		char := rune(str[last-i])
+		if unicode.IsDigit(char) {
+			return int(char - '0')
+		}
+	}
+	return 0
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
