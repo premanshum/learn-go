6b76780e0a9e50dc67f3215fb27c81bd17e7f76d
prem-mukherji <premanshu.mukherji@maersk.comm>
2024-03-25 12:27:43 +0530

    Database is working;

--- CallCenter/backend/src/shared/dbEntity/dbEntity.go
+++
@@ -1,9 +0,0 @@
-package dbEntity
-
-type Team struct {
-	ID          string   `json:"id,omitempty" bson:"id,omitempty"`
-	Name        string   `json:"displayName,omitempty" bson:"displayName,omitempty"`
-	Owners      []string `json:"owners,omitempty" bson:"owners,omitempty"`
-	UpdatedBy   string   `json:"updatedBy,omitempty" bson:"updatedBy,omitempty"`
-	UpdatedDate string   `json:"updatedDate,omitempty" bson:"updatedDate,omitempty"`
-}
--- CallCenter/kubectlCommands.ps1
+++ CallCenter/kubectlCommands.ps1
@@ -12,11 +12,16 @@ kubectl -n cloud-explorer-d delete pvc -l app=mongodb
 kubectl -n cloud-explorer-d delete statefulset mongodb
 kubectl -n cloud-explorer-d delete svc mongo-headless
 kubectl -n cloud-explorer-d delete svc mongo-svc
+kubectl -n cloud-explorer-d get replicaset premteam-replicaset -o yaml
 
 # Run a mongoshell
 kubectl run -it -n cloud-explorer-d mongo-shell --image=mongo --rm -- /bin/bash
 
 # Port forwarding
-kubectl port-forward -n cloud-explorer-d svc/premteams-service 8080:5002
+kubectl port-forward -n cloud-explorer-d svc/premteams-service 8080:8080
+kubectl port-forward -n cloud-explorer-d pod/ccteams-587bbccdb9-6zmvt 8080:8080
 kubectl port-forward -n cloud-explorer-d svc/mongo-svc 27017:27017
 
+kubectl exec -it -n cloud-explorer-d ccteams-5c9c5db54-5b7tp  -- /bin/sh
+
+docker run --rm -it -p 7070:7070/tcp premanshu/team:2
